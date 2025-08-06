13eb699a03f6e6ef95461ae2e5b6f28ec296dd6d
kjirou <kjirou.web@gmail.com>
2025-08-06 09:58:52 +0900

    チュートリアルが完了した状態の全体を追加
    以下のチュートリアルを、最後の Building the Game を除いて完了した時点の状態)
    - https://learn.unity.com/course/roll-a-ball?version=6.0
      - 派生的に行なった以下の子チュートリアルの実行も含む
        - https://learn.unity.com/tutorial/project-setup-processes?version=2022.3
        - https://learn.unity.com/tutorial/get-started-with-the-unity-editor?version=6.0
    
    Building the Gameは、リンク落ちしてて実行できなかった。

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+        if (count >= 8)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+    }
+}
